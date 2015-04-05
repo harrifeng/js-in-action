30beb2550a417abbbcfa56e83008c781c05e61fb
harri feng <harrifeng@gmail.com>
2015-04-05 15:14:26 +0800

    quotes inside a string

---
+++ js-jquery/c02/js/string-with-quotes.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hfeng on 2015/4/5.
+ */
+var title;
+var messge;
+title = "Molly's Special Offers";
+messge = '<a href=\"sale.html\">25% off!</a>';
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+var elNote = document.getElementById('note');
+elNote.innerHTML = messge;
