8f00292f4fb26bc2b8102cd2b949af284da2f2c9
harri feng <harrifeng@gmail.com>
2015-04-05 15:06:52 +0800

    new example

---
+++ js-jquery/c02/js/string-variable.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hfeng on 2015/4/5.
+ */
+var username;
+var message;
+username = 'Molly';
+message = 'See our upcoming range';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNode = document.getElementById('note');
+elNode.textContent = message;
