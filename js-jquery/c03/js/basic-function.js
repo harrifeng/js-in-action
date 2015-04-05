e145288c0a349f85eaf7c2694677e90925ad627e
harri feng <harrifeng@gmail.com>
2015-04-05 16:15:34 +0800

    call at once, not store the function itself

--- js-jquery/c03/js/basic-function.js
+++ js-jquery/c03/js/basic-function.js
@@ -3,6 +3,10 @@
  */
 var msg = 'Sign up to receive our newsletter for 10% off!';
 
+msg = (function() {
+    return "hello world";
+}());
+
 function updateMessage() {
     var el = document.getElementById('message');
     el.textContent = msg;
