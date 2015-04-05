6e2c2b30f6278644292299a02ceb7d7c24fc7337
harri feng <harrifeng@gmail.com>
2015-04-05 16:18:26 +0800

    type function

--- js-jquery/c03/js/basic-function.js
+++ js-jquery/c03/js/basic-function.js
@@ -3,10 +3,11 @@
  */
 var msg = 'Sign up to receive our newsletter for 10% off!';
 
-msg = (function() {
-    return "hello world";
-}());
+function area() {
+    return "size is 100";
+};
 
+msg = area();
 function updateMessage() {
     var el = document.getElementById('message');
     el.textContent = msg;
