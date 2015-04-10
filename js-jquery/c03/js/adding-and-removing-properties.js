d747ea5c6cb494163d2491698d1d76fd3acf042f
harri feng <harrifeng@gmail.com>
2015-04-10 10:21:35 +0800

    really understand the class name

--- js-jquery/c03/js/adding-and-removing-properties.js
+++ js-jquery/c03/js/adding-and-removing-properties.js
@@ -14,6 +14,13 @@ delete hotel.booked;
 var elName = document.getElementById('hotelName');
 elName.textContent = hotel.name;
 
+//////////////////////////////////////////////////////////////////
+// Please be careful that the Poll: has a `space` after it      //
+// What we are doing is add two classes for the elPool          //
+// one is `Poll:`, the other is `false`. You will see following //
+// result afterwards:                                           //
+// <p id="pool" class="Pool: false">Pool</p>                    //
+//////////////////////////////////////////////////////////////////
 var elPool = document.getElementById('pool');
 elPool.className = 'Pool: ' + hotel.pool;
 
