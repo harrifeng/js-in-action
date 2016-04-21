9a69c55e3b45724946c41e711bf2d0dacf193f68
Haoran Feng <haoran.feng@sap.com>
2016-04-21 13:32:01 +0800

    new incoming

---
+++ def/ch02/ex04.js
@@ -0,0 +1,16 @@
+console.log(-Infinity);
+
+console.log(-1/0);
+
+console.log(Number.NEGATIVE_INFINITY);
+
+console.log(-1/0);
+
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// -Infinity                                      //
+// -Infinity                                      //
+// -Infinity                                      //
+// -Infinity                                      //
+////////////////////////////////////////////////////
---
+++ def/ch02/ex06.js
@@ -0,0 +1,18 @@
+console.log(Number.MIN_VALUE);
+
+console.log(Number.MIN_VALUE/2);
+
+console.log(-Number.MIN_VALUE/2);
+
+console.log(-1/Infinity);
+
+console.log(-0);
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// 5e-324                                         //
+// 0                                              //
+// -0                                             //
+// -0                                             //
+// -0                                             //
+////////////////////////////////////////////////////
