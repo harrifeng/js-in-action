d18e6df67cc534637336ae775cf6d456e89282f0
Haoran Feng <haoran.feng@sap.com>
2016-04-21 15:25:34 +0800

    reorg

---
+++ def/ch03/ex08.js
@@ -0,0 +1,18 @@
+console.log(Number.isFinite(NaN));
+console.log(Number.isFinite(Infinity));
+console.log(Number.isFinite(12));
+
+console.log(Number.isNaN(12));
+console.log(Number.isNaN(Infinity));
+console.log(Number.isNaN(NaN));
+
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// false                                          //
+// false                                          //
+// true                                           //
+// false                                          //
+// false                                          //
+// true                                           //
+////////////////////////////////////////////////////
---
+++ def/ch03/ex09.js
@@ -0,0 +1,14 @@
+var zero  = 0;
+var negz = -0;
+console.log(zero === negz);
+console.log(1/zero === 1/negz);
+console.log(1/zero);
+console.log(1/negz);
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// true                                           //
+// false                                          //
+// Infinity                                       //
+// -Infinity                                      //
+////////////////////////////////////////////////////
