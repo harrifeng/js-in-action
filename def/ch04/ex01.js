6f9653bbd07f563a95a8d2ab365718e320b57266
Haoran Feng <haoran.feng@sap.com>
2016-04-22 15:52:51 +0800

    new incoming

---
+++ def/ch04/ex01.js
@@ -0,0 +1,17 @@
+var sparseArray = [1, , , , 5];
+
+console.log(sparseArray);
+
+for (var i = 0; i < sparseArray.length; i ++) {
+    console.log(sparseArray[i]);
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// [ 1, , , , 5 ]                                 //
+// 1                                              //
+// undefined                                      //
+// undefined                                      //
+// undefined                                      //
+// 5                                              //
+////////////////////////////////////////////////////
